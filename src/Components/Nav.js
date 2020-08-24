6e132796d04470e57a09fa13595a100b21120ff5
BoguckiMich <bogucki.mich@gmail.com>
2020-08-24 19:56:14 +0200

    navbar finished

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Nav.css"
 
 function Nav() {
+
+    const [show, handleShow] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                handleShow(true)
+            } else handleShow(false);
+        })
+        return () => {
+            window.removeEventListener("scroll")
+        }
+    }, [])
+
     return (
-        <div className="nav">
+        <div className={`nav ${show && "shadow"}`}>
             <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="Netflix Logo" className="nav__logo" />
             <img src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/bf6e4a33850498.56ba69ac3064f.png" alt="Netflix avatar" className="nav__avatar" />
         </div>
